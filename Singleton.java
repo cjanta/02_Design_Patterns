20c84accaf518b1225b0e413c974f59f46e455ec
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-10-07 11:28:22 +0200

    Add files via upload

---
+++ Singleton.java
@@ -0,0 +1,39 @@
+package com.cc.java;
+
+public class Singleton {
+  
+    /* Objektbildung */
+
+    private static Singleton instance;
+
+    private Singleton(){
+        Helper.ausgabe("Hi, I'm there for you!");
+    }
+
+    public static Singleton getInstance(){
+        if (instance == null) 
+        {
+            instance = new Singleton();
+        }       
+        return instance; 
+    }
+
+
+    /* Funktionalität */
+
+    private int counter;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void actOnEvent() {
+        this.counter ++;
+    }
+
+    
+
+
+
+
+}
