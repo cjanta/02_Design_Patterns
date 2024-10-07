678fda04370c852c4c88634996fddcdebb6c3c60
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-10-07 10:59:07 +0000

    01 / Vorbereitung

---
+++ com/cc/java/Singleton.java
@@ -0,0 +1,30 @@
+package com.cc.java;
+
+
+public class Singleton {
+
+private int myNum;
+
+public Singleton(int myNum) {
+    this.myNum = myNum;
+}
+
+public int getMyNum() {
+    return myNum;
+}
+
+public void setMyNum(int myNum) {
+    this.myNum = myNum;
+}
+
+
+
+
+
+
+
+
+
+
+
+}
