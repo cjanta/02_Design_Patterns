8cb8bcabb69dcaacf57e28ef12c125a2b902fb44
Sven Kanter <svkanter@gmail.com>
2024-10-09 08:54:22 +0200

    ++

--- com/cc/java/singleton/App.java
+++ com/cc/java/singleton/App.java
@@ -1,75 +1,31 @@
-<<<<<<< HEAD
 package com.cc.java.singleton;
+import static com.cc.java.singleton.Helper.out;
 
 public class App {
     
     public static void main(String[] args) {
-
-        Singleton singleton1 = new Singleton(2);
-        output("obj1: " + singleton1);
-        output("myNum: " +  singleton1.getMyNum());
-
-        Singleton singleton2 = new Singleton(3);
-        output("obj1: " + singleton2);
-        output("myNum: " +  singleton2.getMyNum());
-
-   
+        testSingletonClass();
     }
 
-    private static void output(String outputStr) {
-        System.out.println(outputStr);
+    private static void testSingletonClass(){
+        out("testing classname: " + Singleton.class.getName());
+        int sumRefs = 5;
+        String refToCheck = "";
+        for (int i = 0; i < sumRefs; i++) {
+            Singleton obj = Singleton.getInstance();
+            out("# objectRef: " + obj);
+            if (i==0){
+                refToCheck = obj.toString();
+            } 
+            checkReferences(refToCheck,obj);
+        }
     }
 
-
+    private static void checkReferences(String refToCheck, Singleton obj){
+        if (!obj.toString().equals(refToCheck) ){
+            throw new UnsupportedOperationException(Singleton.class.getName() + " >> Singleton Fehler: \"Mehrere Instanzen festgestellt!\"");
+        }
+    }
 
 }
 
-=======
-package com.cc.java.singleton;
-
-public class App {
-    
-    public static void main(String[] args) {
-
-        
-        Singleton obj1 = Singleton.getInstance();
-        Helper.output(obj1);
-        obj1.actOnEvent();
-        obj1.actOnEvent();
-        // Events werden kumuliert ... 0 + 2
-        Helper.output(obj1.getCounter());
-        Helper.output("---------------------");
-        
-        Singleton obj2 = Singleton.getInstance();
-        Helper.output(obj2);
-        obj2.actOnEvent();
-        obj2.actOnEvent();
-        obj2.actOnEvent();
-        // Events werden kumuliert ... 2 + 3
-        Helper.output(obj2.getCounter());
-        Helper.output("---------------------");
-
-
-        Singleton obj3 = Singleton.getInstance();
-        Helper.output(obj3);
-        obj3.actOnEvent();
-        obj3.actOnEvent();
-        obj3.actOnEvent();
-        obj3.actOnEvent();
-        obj3.actOnEvent();
-		
-        // Events werden kumuliert ... 5 + 5
-        Helper.output(obj3.getCounter());
-   
-
-
-
-
-
-
-    }
-
-
-}
-
->>>>>>> f76644cb2a32a4bf80eb88ec5b4d377fd917901c
